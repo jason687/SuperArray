9f9107e61154195e359799321bc61acf9740584a
Jason Zhang <jzhang20@stuy.edu>
2020-11-02 17:07:14 -0500

    Tested. Added get method

--- SuperArray.java
+++ SuperArray.java
@@ -8,11 +8,25 @@ public class SuperArray {
 
   public int size () {
     int count = 0;
-    for (int i = 0; i < data.length; i+=) {
-      if (!(data[i].equals(null))) {
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] != null) {
         count += 1;
       }
     }
     return count;
+  }
+
+  public boolean add (String element) {
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] == null) {
+        data[i] = element;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String get (int index) {
+    return data[index];
   }
 }
