29193de3d2bb7c9492ca7d309643ee628ab19447
Jason Zhang <jzhang20@stuy.edu>
2020-11-07 15:05:03 -0500

    findOverlap

--- Demo.java
+++ Demo.java
@@ -30,7 +30,23 @@ public class Demo {
       del = false;
     }
   }
-  
+
+  public static SuperArray findOverlap (SuperArray a, SuperArray b) {
+    SuperArray overlap = new SuperArray(Math.min(a.size(), b.size()));
+    for (int i = 0; i < a.size(); i++) {
+      for (int j = 0; j < b.size(); j++) {
+        if (a.get(i) != null && b.get(j) != null) {
+          if (a.get(i).equals(b.get(j))) {
+            if (!overlap.contains(a.get(i))) {
+              overlap.add(a.get(i));
+            }
+          }
+        }
+      }
+    }
+    return overlap;
+  }
+
   public static void main (String [] args) {
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -40,5 +56,22 @@ public class Demo {
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+
+    System.out.println();
+    SuperArray a = new SuperArray();
+    a.add("9");
+    a.add("1");
+    a.add("2");
+    a.add("2");
+    a.add("3");
+    a.add("4");
+    SuperArray b = new SuperArray();
+    b.add("0");
+    b.add("4");
+    b.add("2");
+    b.add("2");
+    b.add("9");
+    SuperArray overlap = findOverlap(a, b);
+    System.out.println(overlap);
   }
 }
